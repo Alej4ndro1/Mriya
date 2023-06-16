569a4152c1d854f444b7d4cdabe5a4cb94371f17
Oleksandr Sobchuk <osobchuk1@gmail.com>
2023-06-16 19:01:53 +0200

    created footer for mobile & desktop

---
+++ frontend/src/react-app-env.d.ts
@@ -0,0 +1,17 @@
+declare module '*.png' {
+  const value: any;
+
+  export = value;
+}
+
+declare module '*.svg' {
+  const value: string | undefined;
+
+  export = value;
+}
+
+declare module '*.scss' {
+  const value: any;
+
+  export = value;
+}
