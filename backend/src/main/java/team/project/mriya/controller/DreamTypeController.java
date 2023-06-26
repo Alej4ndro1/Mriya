be5308de8d2845f4cf78a7f462b0f38989427f53
Valentyn Chepiha <valentyn.chepiha@gmail.com>
2023-06-26 23:37:11 +0300

    Refactored dream types controller

--- backend/src/main/java/team/project/mriya/controller/DreamTypeController.java
+++ backend/src/main/java/team/project/mriya/controller/DreamTypeController.java
@@ -1,4 +1,29 @@
 package team.project.mriya.controller;
 
+import java.util.List;
+import java.util.stream.Collectors;
+import io.swagger.annotations.ApiOperation;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import team.project.mriya.dto.DreamTypeResponseDto;
+import team.project.mriya.dto.mapper.impl.DreamTypeResponseMapper;
+import team.project.mriya.service.DreamTypeService;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/api/dream-types")
 public class DreamTypeController {
+    private DreamTypeService dreamTypeService;
+    private DreamTypeResponseMapper mapper;
+
+    @GetMapping
+    @ApiOperation(value = "Get all dream types", notes = "return list with all dream types")
+    public List<DreamTypeResponseDto> getALl() {
+        return dreamTypeService.getAll().stream()
+                .map(d -> mapper.toDto(d))
+                .collect(Collectors.toList());
+    }
+
 }
