2ea0b8a46e124f79dfaaded635bf0cf9b6be182f
Oleksandr Sobchuk <osobchuk1@gmail.com>
2023-07-06 16:21:35 +0200

    linked project to docker

--- frontend/src/components/DreamCatalog/DreamCatalog.tsx
+++ frontend/src/components/DreamCatalog/DreamCatalog.tsx
@@ -1,11 +1,37 @@
 import React from 'react';
+import { DownOutlined } from '@ant-design/icons';
+import type { MenuProps } from 'antd';
+import { Dropdown, Space } from 'antd';
 
 export const DreamCatalog = () => {
+  const items: MenuProps['items'] = [
+    {
+      label: <a href="https://www.antgroup.com">1st menu item</a>,
+      key: '0',
+    },
+    {
+      label: <a href="https://www.aliyun.com">2nd menu item</a>,
+      key: '1',
+    },
+    {
+      label: '3rd menu item',
+      key: '3',
+    },
+  ];
 
   return (
     <div className="dreamCatalog">
       <div className='dreamCatalog__container'>
         <h1 className='dreamCatalog__title'>Dreams</h1>
+
+        <Dropdown menu={{ items }} trigger={['click']}>
+          <a onClick={(e) => e.preventDefault()}>
+            <Space>
+              Click me
+              <DownOutlined />
+            </Space>
+          </a>
+        </Dropdown>
       </div>
     </div >
   );
