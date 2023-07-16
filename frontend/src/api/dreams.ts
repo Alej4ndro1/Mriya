985f58803022c8782aefa966937bcbe36815bba0
Oleksandr Malyshev <oleksandr.malyshev@iogru.com>
2023-07-16 14:52:56 +0700

    Added filters

--- frontend/src/api/dreams.ts
+++ frontend/src/api/dreams.ts
@@ -2,8 +2,14 @@ import { client } from '../utils/fetchDreams';
 import { Dream } from '../types/Dream';
 import { City } from '../types/City';
 
-export const getDreams = () => {
-  return client.get<Dream[]>('/api/dream/all');
+export const getDreams = (params: any = {}) => {
+  const nextParams = new URLSearchParams();
+  Object.keys(params).forEach((key: string) => {
+    if (params[key] && params[key] !== '') {
+      nextParams.append(key, params[key]);
+    }
+  });
+  return client.get<Dream[]>(`/api/dream/all?${nextParams}`);
 };
 
 export const getDreamID = (dreamId: number) => {
@@ -12,8 +18,4 @@ export const getDreamID = (dreamId: number) => {
 
 export const getCitys = () => {
   return client.get<City[]>('/api/city');
-};
-
-export const getFilteredDreams = () => {
-  return client.get<Dream[]>('/api/dream/all?count=9&page=0&cities=6');
 };
